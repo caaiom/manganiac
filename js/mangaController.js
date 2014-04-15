64cce2389a8c9c929a185b3542741c56b466a222
Caio Maia <caaiomaia@gmail.com>
2014-04-15 01:32:51 -0300

    New functions and standards
    Added insert function, cross domain request in server, standardized the
    controller, added the bodyparser

--- js/mangaController.js
+++ js/mangaController.js
@@ -1,23 +1,28 @@
 function MangaController($scope, $http) {
+
+    function Manga()  {
+        this.title = '';
+        this.author = '';
+        this.currentVolume = 0;
+        this.lastVolumePurchased = 0;
+        this.rate = 0;
+        this.publisher = '';
+        this.buyImportance = 0;
+        this.price = 0;
+        this.finished = false;
+        this.linkToDescription = '';
+    }
     
     $scope.listEditions = function () {
-        $http.get('http://localhost:1337/getAllItens').success(function (retorno) {
-            $scope.itens = retorno.mangas;
+        $http.get('http://localhost:1337/api/getAllItens').success(function (retorno) {
+            $scope.mangas = retorno.mangas;
         });
     };
 
     $scope.insertEdition = function() {
-        $scope.editions.push({
-            title: $scope.edition.title,
-            author: $scope.edition.author,
-            currentVolume: $scope.edition.currentVolume,
-            lastVolumePurchased: $scope.edition.lastVolumePurchased,
-            rate: $scope.edition.rate,
-            publisher: $scope.edition.publisher,
-            buyImportance: $scope.edition.buyImportance,
-            price: $scope.edition.price,
-            finished: $scope.edition.finished,
-            linkToDescription: $scope.edition.linkToDescription
+        $http.post('http://localhost:1337/api/insertItem', $scope.manga).success(function() {
+            $scope.mangas.push($scope.manga);
+            $scope.manga = new Manga();
         });
     }
 
